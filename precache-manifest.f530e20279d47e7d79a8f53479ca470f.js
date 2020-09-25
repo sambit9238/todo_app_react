0d54e64cfefa033e1d98ec1037762b200f67d020
sambit9238 <sambit9238@gmail.com>
2020-09-25 23:40:58 +0530

    Updates

---
+++ precache-manifest.f530e20279d47e7d79a8f53479ca470f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47e785fc7afc69181027375fea0911d",
+    "url": "/todo_app_react/index.html"
+  },
+  {
+    "revision": "aad2c891be450f938841",
+    "url": "/todo_app_react/static/css/main.392f804c.chunk.css"
+  },
+  {
+    "revision": "77f6b516bda1bdf383c0",
+    "url": "/todo_app_react/static/js/2.cee7f66d.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/todo_app_react/static/js/2.cee7f66d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad2c891be450f938841",
+    "url": "/todo_app_react/static/js/main.9de9658e.chunk.js"
+  },
+  {
+    "revision": "2925bf734b3f3ab957f5",
+    "url": "/todo_app_react/static/js/runtime-main.492a04f2.js"
+  }
+]);
